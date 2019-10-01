d15fd4be09c1f6b64ca13a7fc6ca19cf5ed98afb
turajarmin <turajarminpour98@gmail.com>
2019-10-01 07:06:26 -0700

    =add all file in the project

---
+++ about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ex nemo placeat asperiores ullam iste dolores quos doloribus reprehenderit amet recusandae ab corporis illo, quas culpa qui saepe minima similique.
+        Qui officiis quia voluptatum repellat, doloremque quas ullam! Sapiente reprehenderit quia perspiciatis ipsam nostrum saepe esse ex dolorem laboriosam placeat. Praesentium perspiciatis alias saepe, expedita fuga voluptatem vero nesciunt molestias?
+        Qui sit laboriosam autem. Fuga numquam, maiores vero deleniti ullam labore officiis. Nisi nesciunt consequuntur doloremque dicta, excepturi deleniti voluptatum veritatis ducimus. Quia provident iure saepe eveniet deserunt similique deleniti.
+        Dolore modi optio nam tenetur quo. Id doloribus debitis nostrum quae excepturi quas iste at recusandae harum, numquam amet veritatis ea tenetur reiciendis cumque aliquid eveniet culpa ratione. Quam, velit.
+        Voluptates vel facere, sequi eaque in corporis, sapiente cumque magnam odio quidem quia, voluptas corrupti animi molestias exercitationem nam incidunt? Mollitia, magni unde iusto libero quos similique inventore omnis ex!</p>
+    </section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ratione illo nihil ex nostrum debitis voluptas officia beatae quidem quam corrupti impedit, ducimus maiores aliquid alias quos magni eius consequuntur?</p>
+</body>
+</html>   

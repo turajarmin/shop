cc4e3b4e057d43ec6d27522b942801efd4345847
turajarmin <turajarminpour98@gmail.com>
2019-10-01 07:12:14 -0700

    =change index.php

--- index.php
+++ index.php
@@ -8,12 +8,11 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ratione illo nihil ex nostrum debitis voluptas officia beatae quidem quam corrupti impedit, ducimus maiores aliquid alias quos magni eius consequuntur?</p>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem at cupiditate nemo quae recusandae corporis neque atque corrupti eos expedita incidunt dolorem quo odio architecto esse, reprehenderit aut quibusdam autem?</p>
-            <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus odio cupiditate ex architecto doloribus accusantium maiores, ipsa, facilis veniam quaerat numquam. Atque, suscipit. Assumenda labore repellendus quibusdam, eos quisquam praesentium?
-            </p>
+            <section>
+                <h1>hello world</h1>
+                <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, modi debitis dolores dicta tempore maiores nostrum odio consequatur ipsa laudantium? Ab laborum, eveniet excepturi esse assumenda necessitatibus deserunt a nihil?</p>
+                <a href="#">read-more</a>
+            </section>
 </body>
 </html>   
